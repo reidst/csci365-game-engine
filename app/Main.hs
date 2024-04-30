88f3f69ca3811239fb36d83a9129f91bb99d353f
Katherine <reidst@hendrix.edu>
2024-04-30 16:46:31 -0500

    example vty

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,18 @@
 module Main where
 
-import qualified MyLib (someFunc)
+-- import qualified MyLib (someFunc)
+
+import Graphics.Vty
+import Graphics.Vty.CrossPlatform (mkVty)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+    vty <- mkVty defaultConfig
+    let line0 = string (defAttr `withForeColor` green) "first line"
+        line1 = string (defAttr `withBackColor` blue) "second line"
+        img = line0 <-> line1
+        pic = picForImage img
+    update vty pic
+    e <- nextEvent vty
+    shutdown vty
+    print ("Last event was: " ++ show e)
